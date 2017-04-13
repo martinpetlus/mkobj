984cbcbec5a99a645ee6e76e4b06be8528187d3c
Martin Petluš <martinpetlus@gmail.com>
2017-04-13 18:43:00 +0200

    Add preset env and base impl

---
+++ src/mkobj.js
@@ -0,0 +1,15 @@
+export default function mkobj(obj, ...props) {
+  if (Array.isArray(obj)) {
+    props.unshift(obj);
+
+    // eslint-disable-next-line no-param-reassign
+    obj = {};
+  }
+
+  props.forEach((prop) => {
+    if (prop[0]) {
+      // eslint-disable-next-line no-param-reassign
+      obj[prop[1]] = prop[2];
+    }
+  });
+}
